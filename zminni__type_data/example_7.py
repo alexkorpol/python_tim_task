95529b548f2afac8d4b8fb819f34bf20dcea1400
Alexander <alexkorpol1964@gmail.com>
2023-12-07 11:55:29 +0200

    change directory zminni__type_data

---
+++ zminni__type_data/example_7.py
@@ -0,0 +1,15 @@
+# Напишіть програму, яка отримує такі дані: ім’я, вік, хобі, введені з клавіатури (вводяться на окремих рядках), і друкує на екрані одним повідомленням повну інформацію на основі введених даних.
+
+# Вхідні дані:
+
+# Lord Voldemort
+# 72
+# Magic
+# Вихідні дані:
+
+# My name is Lord Voldemort. I am 72 and my hobby is Magic
+
+name = input("Введіть Ваше ім'я ")
+years = int(input("Введіть ваш вік "))
+hobby = input("Введіть Ваше хоббі ")
+print(f"My name is {name}. I am {years} and my hobby {hobby}. ")
